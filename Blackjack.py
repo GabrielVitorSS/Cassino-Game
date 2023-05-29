085d2939d65644cc21be3c65ed385f83a0a543a3
Leonardo Guedes <Leonardo.c.c.guedes@gmail.com>
2023-05-29 20:54:20 -0300

    Finalização Blackjack e ajustes Interface

--- Blackjack.py
+++ Blackjack.py
@@ -2,59 +2,69 @@ import random
 
 
 class Blackjack:
-
     def __init__(self):
         self.num_decks = 6
         self.cartas = ['A', '2', '3', '4', '5',
                        '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         self.mao_dealer = []
 
-    def iniciar_jogo(self):
-        print('***********************')
+    def iniciar_jogo(self, jogadores):
+        print('\n\n\n\n\n\n\n***********************')
         print('Bem-vindo ao Blackjack!')
         print('***********************\n')
+        self.fazer_apostas(jogadores)
+        self.distribuir_cartas(jogadores)
 
-        num_jogadores = int(
-            input('Quantos jogadores estão participando?'))
-        self.distribuir_cartas(num_jogadores)
+    def fazer_apostas(self, jogadores):
+        for jogador in jogadores:
+            nome_jogador = jogador[0]
+            saldo = jogador[1]
+            aposta = float(input(f'Jogador {nome_jogador}, faça sua aposta (saldo disponível: R${saldo}): '))
+            while aposta > saldo:
+                print('Saldo insuficiente. Digite um valor válido.')
+                aposta = float(input(f'Jogador {nome_jogador}, faça sua aposta (saldo disponível: R${saldo}): '))
+            jogador.append(aposta)
 
-    def distribuir_cartas(self, num_jogadores):
+    def distribuir_cartas(self, jogadores):
         maos_jogadores = []
-        for jogador in range(num_jogadores):
+        for jogador in jogadores:
+            nome_jogador = jogador[0]
             mao = random.sample(self.cartas, 2)
-            maos_jogadores.append(mao)
-            print(f'Jogador {jogador + 1}: {mao}')
+            maos_jogadores.append([nome_jogador, mao])
+            print(f'Jogador {nome_jogador}: {mao}')
 
         self.mao_dealer = random.sample(self.cartas, 2)
         self.distribuir_cartas_dealer()
         self.jogar_rodada(maos_jogadores)
         self.jogar_rodada_dealer()
-        self.resultado_maos(maos_jogadores)
+        self.resultado_maos(maos_jogadores, jogadores)
+        self.mostrar_saldos(jogadores)
+        self.jogar_novamente(jogadores)
 
     def distribuir_cartas_dealer(self):
         print(f'Dealer: [{self.mao_dealer[0]}, Carta oculta]')
 
     def jogar_rodada(self, maos_jogadores):
-        for jogador, mao in enumerate(maos_jogadores):
+        for mao in maos_jogadores:
+            nome_jogador = mao[0]
+            mao = mao[1]
             while True:
-                possibilidade = input(
-                    f'Jogador {jogador + 1}, suas cartas são: {jogador + 1}: {mao}, você deseja mais uma carta? (s/n): ')
+                possibilidade = input(f'Jogador {nome_jogador}, suas cartas são: {mao}, você deseja mais uma carta? (s/n): ')
                 if possibilidade.lower() == 's':
                     carta = random.choice(self.cartas)
                     mao.append(carta)
-                    print(f'Jogador {jogador + 1}: {mao}')
+                    print(f'Jogador {nome_jogador}: {mao}')
                     somatorio = self.calcular_somatorio(mao)
                     if somatorio > 21:
-                        print(f'Jogador {jogador + 1} estourou!')
+                        print(f'Jogador {nome_jogador} estourou!')
                         break
                     elif somatorio == 21:
-                        print(f'Jogador {jogador + 1} fez Blackjack!')
+                        print(f'Jogador {nome_jogador} fez Blackjack!')
                         break
                 elif possibilidade.lower() == 'n':
                     break
                 else:
-                    print(
-                        'Opção inválida! Digite "s" para mais uma carta ou "n" para parar.')
+                    print('Opção inválida! Digite "s" para mais uma carta ou "n" para parar.')
 
     def calcular_somatorio(self, mao):
         somatorio = 0
@@ -86,29 +96,79 @@ class Blackjack:
             print('Dealer: ', self.mao_dealer)
             print('Somatório do dealer é: ', somatorio_dealer)
 
-    def resultado_maos(self, maos_jogadores):
-        for jogador, mao in enumerate(maos_jogadores):
+    def resultado_maos(self, maos_jogadores, jogadores):
+        for mao in maos_jogadores:
+            nome_jogador = mao[0]
+            mao = mao[1]
             somatorio = self.calcular_somatorio(mao)
             if somatorio > 21:
-                print(f'Jogador {jogador + 1} estourou!')
+                print(f'Jogador {nome_jogador} estourou!')
+                self.pagar_perdedor(nome_jogador, jogadores)
             elif somatorio == 21:
-                print(f'Jogador {jogador + 1} fez Blackjack!')
+                print(f'Jogador {nome_jogador} fez Blackjack!')
+                self.pagar_ganhador(nome_jogador, jogadores)
             else:
                 somatorio_dealer = self.calcular_somatorio(self.mao_dealer)
                 if somatorio_dealer > 21:
-                    print(f'Jogador {jogador + 1} ganhou!')
+                    print(f'Jogador {nome_jogador} ganhou!')
+                    self.pagar_ganhador(nome_jogador, jogadores)
                 elif somatorio_dealer == 21:
-                    print(f'Jogador {jogador + 1} perdeu!')
+                    print(f'Jogador {nome_jogador} perdeu!')
+                    self.pagar_perdedor(nome_jogador, jogadores)
                 elif somatorio > somatorio_dealer:
-                    print(f'Jogador {jogador + 1} ganhou!')
+                    print(f'Jogador {nome_jogador} ganhou!')
+                    self.pagar_ganhador(nome_jogador, jogadores)
                 elif somatorio < somatorio_dealer:
-                    print(f'Jogador {jogador + 1} perdeu!')
+                    print(f'Jogador {nome_jogador} perdeu!')
+                    self.pagar_perdedor(nome_jogador, jogadores)
                 elif somatorio == somatorio_dealer:
-                    print(f'Jogador {jogador + 1} empatou!')
+                    print(f'Jogador {nome_jogador} empatou!')
+                    self.pagar_empate(nome_jogador, jogadores)
                 else:
                     print('Erro inesperado!')
 
+    def pagar_ganhador(self, nome_jogador, jogadores):
+        for jogador in jogadores:
+            if jogador[0] == nome_jogador:
+                aposta = jogador[2]
+                saldo_atual = jogador[1]
+                saldo_atual += aposta * 2
+                jogador[1] = saldo_atual
+                print(f'Jogador {nome_jogador} ganhou {aposta * 2}!')
 
-jogo = Blackjack()
+    def pagar_perdedor(self, nome_jogador, jogadores):
+        for jogador in jogadores:
+            if jogador[0] == nome_jogador:
+                aposta = jogador[2]
+                saldo_atual = jogador[1]
+                saldo_atual -= aposta
+                jogador[1] = saldo_atual
+                print(f'Jogador {nome_jogador} perdeu {aposta}!')
 
-jogo.iniciar_jogo()
+    def pagar_empate(self, nome_jogador, jogadores):
+        for jogador in jogadores:
+            if jogador[0] == nome_jogador:
+                aposta = jogador[2]
+                saldo_atual = jogador[1]
+                jogador[1] = saldo_atual
+                print(f'Jogador {nome_jogador} empatou! A aposta foi devolvida.')
+
+    def mostrar_saldos(self, jogadores):
+        print('\nSaldo final:')
+        for jogador in jogadores:
+            nome_jogador = jogador[0]
+            saldo = jogador[1]
+            print(f'Jogador {nome_jogador}: R${saldo}')
+
+    def jogar_novamente(self, jogadores):
+        resposta = input('\nDeseja jogar novamente? (s/n): ')
+        if resposta.lower() == 's':
+            for jogador in jogadores:
+                jogador[2] = 0  # Zera a aposta dos jogadores
+            self.iniciar_jogo(jogadores)
+        else:
+            print('Obrigado por jogar!')
+
+    def jogar(self, jogadores):
+        self.iniciar_jogo(jogadores)
+
--- Interface.py
+++ Interface.py
@@ -12,29 +12,40 @@ class Interface:
     def criar_jogadores(self):
         continuar = True
         while continuar:
-            nome = input('Digite o nome do jogador: ')
+            nome = input('\nDigite o nome do jogador: ')
             deposito = float(input('Digite o valor do depósito: '))
 
             self.jogadores.append([nome, deposito])
 
-            resposta = input('Deseja adicionar outro jogador? (s/n): ')
+            resposta = input('\nDeseja adicionar outro jogador? (s/n): ')
             if resposta.lower() != 's':
                 continuar = False
 
     def mostrar_jogadores(self):
-        print('Jogadores registrados:')
+        print('\n \n \nJogadores registrados:')
         for jogador in self.jogadores:
             print(f'Nome: {jogador[0]}, Saldo: R${jogador[1]}')
 
+    def fazer_apostas(self):
+        for jogador in self.jogadores:
+            nome_jogador = jogador[0]
+            saldo = jogador[1]
+            aposta = float(input(f'Jogador {nome_jogador}, faça sua aposta (saldo disponível: R${saldo}): '))
+            while aposta > saldo:
+                print('Saldo insuficiente. Digite um valor válido.')
+                aposta = float(input(f'Jogador {nome_jogador}, faça sua aposta (saldo disponível: R${saldo}): '))
+            jogador.append(aposta)
+
     def escolher_jogo(self):
-        print('Escola o Jogo: ')
+        print('\nEscolha o Jogo: ')
         print('R - Roleta')
         print('B - Blackjack')
         print('Caso não deseje apostar, digite "S" para sair e seu saldo será devolvido.')
         jogo = input('Digite a opção desejada: ')
         if jogo.lower() == 'b':
-            blackjack = Blackjack.Blackjack()
-            blackjack.iniciar_jogo()
+            game = Blackjack.Blackjack()
+            game.jogar(self.jogadores)
+
 
 
 interface = Interface()
